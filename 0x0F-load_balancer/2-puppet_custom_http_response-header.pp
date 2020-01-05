fd8953c02da693beac936b19d768f6de121ccbba
lucho1503 <lmmc.1503@gmail.com>
2020-01-05 03:40:12 +0000

    2-puppet_custom_http_response-header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# add header custom using puppet
+
+exec {'install nginx':
+  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo sed -i "15i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf && sudo service nginx restart',
+  provider => shell,
+  }
