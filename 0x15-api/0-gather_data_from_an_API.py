e6f40f6081e8f1b0b816c241e26897b10f342c5b
lucho1503 <lmmc.1503@gmail.com>
2020-02-04 15:41:41 +0000

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" this script retrieves information about the progress of a list """
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    req = 'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1])
+    response = requests.get(req)
+    resp_json = response.json()['name']
+
+    url_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    to_dos = url_todos.json()
+    total_task = []
+    num_task = 0
+    completed_task = 0
+
+    for ta in to_dos:
+        if ta.get('userId') == int(sys.argv[1]):
+            num_task += 1
+
+            if ta.get('completed'):
+                total_task.append(ta['title'])
+                completed_task += 1
+
+    print("Employee {} is done with task({}/{}):".
+          format(resp_json, num_task, completed_task))
+    for ta in total_task:
+        print("\t {}".format(ta))
