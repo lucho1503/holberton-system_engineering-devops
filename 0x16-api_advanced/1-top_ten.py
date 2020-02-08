3ef17dad509769a906ddabb47a2e29fe3f2e5775
lucho1503 <lmmc.1503@gmail.com>
2020-02-08 03:19:57 +0000

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" this script retrieves a top ten first post of a reddit given """
+
+import requests
+
+
+def top_ten(subreddit):
+    """ first top ten subreddits post """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    header = {'User-Agent': 'Chrome'}
+    r = requests.get(url, headers=header, allow_redirects=False)
+    if r.status_code == 200:
+        for data in r.json().get('data').get('children'):
+            print (data.get('data').get('title'))
+    else:
+        print(None)
