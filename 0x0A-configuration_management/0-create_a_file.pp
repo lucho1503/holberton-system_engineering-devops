77397c04e7b2dceb42ecb9b5491ca0ef1bb3a94e
lucho1503 <lmmc.1503@gmail.com>
2020-03-23 23:01:07 +0000

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,5 @@
-#create a file in the path /tmp/holberton
-file { '/tmp/holberton':
+#create a new file
+file {'/tmp/holberton':
   mode	  => '0744',
   owner	  => 'www-data',
   group	  => 'www-data',
