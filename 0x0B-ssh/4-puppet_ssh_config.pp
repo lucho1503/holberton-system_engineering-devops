19821912f27042689c97dcecbdccfbda158c038b
lucho1503 <lmmc.1503@gmail.com>
2020-03-24 16:42:16 +0000

    add ~

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # client configuration file connect to a server without typing a password
 exec { 'echo':
   path    => 'usr/bin:/bin',
-  command => 'echo "	IdentityFile /.ssh/holberton\n	 PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  command => 'echo "	IdentityFile ~/.ssh/holberton\n	 PasswordAuthentication no" >> /etc/ssh/ssh_config',
   returns => [0,1],
 }
