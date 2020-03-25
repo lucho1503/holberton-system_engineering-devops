55829ac3773e895cbdcce73096781f4423ae82ae
lucho1503 <lmmc.1503@gmail.com>
2020-03-25 21:37:40 +0000

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 #install ngnix with puppet and configurate a server
 exec { 'install':
+  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && echo "Holberton School" > /var/www/html/index.nginx-debian.html && n_s="\\\trewrite ^/redirect_me https://www.youtube.com/ permanent;" && sudo sed -i "42i $n_s" /etc/nginx/sites-available/default && sudo service nginx start',
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=EshjSGSg0q0&t=35s permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
